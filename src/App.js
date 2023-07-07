3de060ffd0db0ade0e8623475fa877e33cbe2190
Tecnologías de la Información <tic.calama@inacap.cl>
2023-07-07 14:37:59 -0400

    Versión Inicial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useSate} from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <h1>Nueva página de inicio</h1>
+      <p>Hola desde aquí</p>
+    </>
+  )
+}
+
+export default App;
